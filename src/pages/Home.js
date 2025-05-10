d9763c1dd5c6726685c8a385090f627d269895a3
mudunkotuwasb <110487612+mudunkotuwasb@users.noreply.github.com>
2025-05-10 13:21:28 +0530

    Implement Infinite Scroll or Load More

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar';
 import MovieGrid from '../components/MovieGrid';
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+
+  const fetchMovies = async (searchQuery, pageNumber) => {
+    const response = await searchMovies(searchQuery, pageNumber);
+    setMovies((prevMovies) => [...prevMovies, ...response.results]);
+  };
+
   const handleSearch = (query) => {
     console.log('Search query:', query);
-    // Add logic to handle search query
+    setQuery(query);
+    setPage(1);
+    setMovies([]);
+    fetchMovies(query, 1);
   };
+
+  const loadMoreMovies = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+    fetchMovies(query, nextPage);
+  };
+
+  useEffect(() => {
+    if (query) {
+      fetchMovies(query, page);
+    }
+  }, [query, page]);
 
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
-      <MovieGrid movies={[]} />
+      <MovieGrid movies={movies} />
+      <button onClick={loadMoreMovies}>Load More</button>
       Home Page
     </div>
   );
